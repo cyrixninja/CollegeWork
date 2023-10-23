adbf9da4a047463556ee874ec024f82c2b6d2dcf
Harsh Kumar <61012869+cyrixninja@users.noreply.github.com>
2023-10-23 10:24:33 +0530

    Update

--- 3rd Sem/DS/2_10.c
+++ 3rd Sem/DS/2_10.c
@@ -34,63 +34,27 @@ struct Node* insert(struct Node* root, int value) {
 
 // Preorder Traversal
 void preorder(struct Node* root) {
-    struct Node* stack[100]; 
-    int top = -1;
     if (root == NULL) return;
-
-    stack[++top] = root;
-
-    while (top >= 0) {
-        struct Node* current = stack[top--];
-        printf("%d ", current->data);
-
-        if (current->right) stack[++top] = current->right;
-        if (current->left) stack[++top] = current->left;
-    }
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
 }
 
 
 // Postorder Traversal
 void postorder(struct Node* root) {
     if (root == NULL) return;
-    
-    struct Node* stack1[100];
-    struct Node* stack2[100];
-    int top1 = -1;
-    int top2 = -1;
-    stack1[++top1] = root;
-
-    while (top1 >= 0) {
-        struct Node* current = stack1[top1--];
-        stack2[++top2] = current;
-
-        if (current->left) stack1[++top1] = current->left;
-        if (current->right) stack1[++top1] = current->right;
-    }
-
-    while (top2 >= 0) {
-        printf("%d ", stack2[top2--]->data);
-    }
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
 }
 
 // Inorder Traversal
 void inorder(struct Node* root) {
-    struct Node* stack[100]; 
-    int top = -1;
-    struct Node* current = root;
-
-    while (1) {
-        while (current != NULL) {
-            stack[++top] = current;
-            current = current->left;
-        }
-
-        if (top == -1) break;
-
-        current = stack[top--];
-        printf("%d ", current->data);
-        current = current->right;
-    }
+    if (root == NULL) return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
 }
 
 

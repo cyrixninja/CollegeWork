4bb61da8b96cd2dba52de83ec70ae8761d56cf57
cyrixninja <cyrixninja@outlook.com>
2024-03-03 08:31:54 +0530

    Add factorial calculation script in 5.sh

--- 4th Sem/OS/5.sh
+++ 4th Sem/OS/5.sh
@@ -1,2 +1,19 @@
 # Write a shell script to find factorial of given number n
 # !/bin/bash
+
+echo "Enter a number"
+read n
+
+fact=1
+for((i=1;i<=n;i++))
+do
+  fact=$((fact * i))
+done
+
+echo "Factorial of $n is $fact"
+
+# Output
+# sh 5.sh
+# Enter a number
+# 5
+# Factorial of 5 is 120

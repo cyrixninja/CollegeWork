a54e82f701ec406d3e20be521dbbb493f1d7d40c
Harsh Kumar <61012869+cyrixninja@users.noreply.github.com>
2024-03-13 16:15:26 +0530

    Update 9.sh

--- 4th Sem/OS/9.sh
+++ 4th Sem/OS/9.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Write a shell script to read n numbers as command arguments and sort them in descending order.
-# !/bin/bash
 
-echo "Enter the number of elements: "
-read -r n
+n=$#
 echo "Enter the elements: "
 for ((i=0;i<n;i++))
 do
-    read -r arr[$i]
+    arr[$i]=$1
+    shift
 done
+
 for ((i=0;i<n;i++))
 do
     for ((j=0;j<n-i-1;j++))
@@ -26,15 +26,3 @@ for ((i=0;i<n;i++))
 do
     echo -n "${arr[$i]} "
 done
-
-# Output
-# Enter the number of elements:
-# 5
-# Enter the elements:
-# 5
-# 4
-# 3 
-# 9
-# 8
-# The sorted array in descending order is:
-# 9 8 5 4 3

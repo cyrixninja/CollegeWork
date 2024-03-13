067dd8956f3a4b524637b6514a801a54594caaf2
Harsh Kumar <61012869+cyrixninja@users.noreply.github.com>
2024-03-13 16:17:13 +0530

    Update 16.sh

--- 4th Sem/OS/16.sh
+++ 4th Sem/OS/16.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
 # Write a program for process creation using C. (Use of gcc compiler).
 
-
-echo "Enter the file name"
-read file
-if [ -f $file ]
-then
-    echo "File found"
-    echo "Enter the process name"
-    read process
-    if [ -z $process ]
-    then
-        echo "No process found"
-    else
-        echo "Process found"
-    fi
-else
-    echo "File not found"
-fi

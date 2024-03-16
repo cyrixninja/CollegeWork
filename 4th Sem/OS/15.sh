9c41ee56ce428ae446da0b3297bad68da6c82261
Harsh Kumar <61012869+cyrixninja@users.noreply.github.com>
2024-03-16 17:33:11 +0530

    Update 15.sh

--- 4th Sem/OS/15.sh
+++ 4th Sem/OS/15.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 # Write an awk program using function, which convert each word in a given text into capital.
+
+# Define the text
+echo "Please enter some text:"
+read sampletxt
+
+# Use awk to convert each word to uppercase
+echo $sampletxt | awk '{ 
+    for (i=1; i<=NF; i++) {
+        $i = toupper($i)
+    }
+    print $0
+}'

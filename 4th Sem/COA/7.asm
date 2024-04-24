8e920835640606df5435e396fbc79a533deedc0d
Harsh Kumar <cyrixninja@outlook.com>
2024-04-24 13:39:11 +0530

    Fix Issues in 7.asm

--- 4th Sem/COA/7.asm
+++ 4th Sem/COA/7.asm
@@ -9,17 +9,17 @@ jmp start
 
 ;code
 start: nop
-	LXI H,8000H; Point to Starting Location
-	MOV C,H; Take Size of the Array in Counter C
-	LXI D,9000H; Point to Address Location
-	MOV C,A;
+	LXI H,8000H; Point to 8000H to get Block Size
+	MOV C,M; Take Size of the Block in Counter C
+	LXI D,9000H; Point to Destination Location
+	MOV A,L; Load L into A
+	ADD C; Add C to point to last address of block
 	MOV L,A; Move A to L
 
 LOOP:	MOV A,M;Load Memory to A 
-	STAX D; Store A into Destination pointed by DE
-	INX D;Point destination to next location
-	DCX H; Point source to previous address
-	DCR C; Decrease C by 1
-	JNZ LOOP;
-	STAX D;
+		STAX D; Store A into Destination pointed by DE
+		INX D;Point destination to next location
+		DCX H; Point source to previous address
+		DCR C; Decrease C by 1
+		JNZ LOOP; If Z not set then jump to loop
 hlt

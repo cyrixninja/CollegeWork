f657656fb370281c1e1ef1d5f17437626942ffa0
Harsh Kumar <61012869+cyrixninja@users.noreply.github.com>
2024-05-14 18:24:41 +0530

    Made it Easier

--- 4th Sem/OS/7.sh
+++ 4th Sem/OS/7.sh
@@ -2,25 +2,18 @@
 # Write a shell script which will generate first n fibonnacci numbers like: 1, 1, 2, 3, 5, 13, ...
 
 
-echo "Enter the value of n :"
+echo "Enter Value of N"
 read -r n
-echo "The first $n fibonacci numbers are:"
+
+echo "The Fibonacci Series is :"
+
 a=0
 b=1
-count=0
-while [ $count -lt $n ]
+
+for ((i = 0; i < n; i++))
 do
-echo -n "$a "
-fn=`expr $a + $b`
-a=$b
-b=$fn
-count=`expr $count + 1`
+    echo "$a"
+    temp=$((a + b))
+    a=$b
+    b=$temp
 done
-
-# Output
-# Enter the value of n :
-# 5
-# The first 5 fibonacci numbers are:
-# 0 1 1 2 3 %   
-
-

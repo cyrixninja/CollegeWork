00f3c2212948461677d0dfda2bb0b74687c5613c
Harsh Kumar <61012869+cyrixninja@users.noreply.github.com>
2024-08-09 04:20:57 +0000

    Update ADA Practicals

--- 5th Sem/ADA/2_2.py
+++ 5th Sem/ADA/2_2.py
@@ -3,7 +3,7 @@
 #Importing Time Module for Execution Time Analysis
 import time
 
-# Record start time
+# Record start time for iterative method
 start = time.time()
 
 #Take Number of elements as input from the user
@@ -14,7 +14,7 @@ arr = []
 for i in range(n):
     arr.append(int(input("Enter the element: ")))
 
-# Binary Search Algorithm
+# Binary Search Algorithm (Iterative Method)
 def BinarySearch(arr, n, element):
     low = 0
     high = n - 1
@@ -40,10 +40,52 @@ def BinarySearch(arr, n, element):
 
 
 # Array Found at Index
-print("Element Found at Indeelement: ", BinarySearch(arr, n, int(input("Enter the element to search: "))))
+print("Element Found at Index: ", BinarySearch(arr, n, int(input("Enter the element to search: "))))
 
-# Record end time 
-end = time.time()
+# Record end time iterative method
+end = time.time() 
 
-# Print the Eelementecution Time
-print("The time of execution of above program is :",(end-start) * 10**3, "ms")
+# Print the Execution Time
+print("The time of execution of above program using Iterative Method is :",(end-start) * 10**3, "ms")
+
+
+# Record start time for Recursive method
+start = time.time()
+
+#Take Number of elements as input from the user
+n = int(input("Enter the number of elements: "))
+
+# Take the Elements in the Array from the user
+arr = []
+for i in range(n):
+    arr.append(int(input("Enter the element: ")))
+
+# Binary Search Algorithm (Recursive Method)
+def BinarySearchRecursive(arr, n, element):
+    if n >= 1:
+        mid = n // 2
+
+        # If element is present at the middle itself
+        if arr[mid] == element:
+            return mid
+
+        # If element is smaller than mid, then it can only be present in left subarray
+        elif arr[mid] > element:
+            return BinarySearchRecursive(arr, mid, element)
+
+        # Else the element can only be present in right subarray
+        else:
+            return mid + BinarySearchRecursive(arr[mid + 1:], n - mid - 1, element)
+
+    # We reach here when the element is not present in the array
+    return -1
+
+
+# Array Found at Index
+print("Element Found at Index: ", BinarySearchRecursive(arr, n, int(input("Enter the element to search: "))))
+
+# Record end time Recursive method
+end = time.time() 
+
+# Print the Execution Time
+print("The time of execution of above program using Recursive Method is :",(end-start) * 10**3, "ms")
--- 5th Sem/ADA/2_4.py
+++ 5th Sem/ADA/2_4.py
@@ -1,1 +1,26 @@
 # Implmentation and Time Analysis of Quick Sort
+
+#Importing Time Module for Execution Time Analysis
+import time
+
+# Record start time
+start = time.time()
+
+#Take Number of elements as input from the user
+n = int(input("Enter the number of elements: "))
+
+# Take the Elements in the Array from the user
+arr = []
+for i in range(n):
+    arr.append(int(input("Enter the element: ")))
+
+# Quick Sort Algorithm
+
+# Printing the Sorted Array
+print("Sorted Array: ", arr)
+
+# record end time
+end = time.time()
+
+# Print the Execution Time
+print("The time of execution of above program is :",(end-start) * 10**3, "ms")

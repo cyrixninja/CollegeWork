f082c51a34c51c8def5889d578077af62801bb45
Harsh Kumar <61012869+cyrixninja@users.noreply.github.com>
2025-02-24 12:18:05 +0530

    Update 6.asm

--- 6th sem/MI/6.asm
+++ 6th sem/MI/6.asm
@@ -1,15 +1,21 @@
 ;Check whether the number stored in register B is positive, negative or zero. 
 ;The accumulator is 00H if number is zero, 11H if number is positive and 22H if number is negative.
-JMP start
-
-start:	MVI B , 05H ; B --> 05H
+	
+	MVI B , 0BH;
 	MOV A , B;
+	CMA
+	INR A ;
+	CPI 00H;
+	JZ zero
+	ANI 80H;
+	JZ positive
+	JNZ negative
 
+zero:	MVI A,0H;
 
-zero:		MVI  A, 00H
+positive:	MVI A, 11H;
 
-positive:	MVI A, 11H
+negative:	MVI A , 22H;	
 
-negative:	MVI A, 22H
 		
 	HLT	
